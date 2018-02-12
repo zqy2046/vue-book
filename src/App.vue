d0fd13ca19caf591dfbea374e2dbfc2ffffe8a78
zqy2046 <qingyuan_zhang2046@hotmail.com>
2018-02-12 22:17:21 +0800

    transition

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
     <tab></tab>
-    <transition :name="names" >
+    <transition :name="names">
       <keep-alive>
         <router-view v-if="$route.meta.keepAlive"></router-view>
       </keep-alive>
     </transition>
-    <transition :name="names" >
+    <transition :name="names">
       <router-view v-if="!$route.meta.keepAlive"></router-view>
     </transition>
   </div>
@@ -15,23 +15,23 @@
 <script>
   import Tab from './base/Tab'
   import BookHeader from './base/MHeader'
-  
+
   export default {
-    
+
     name: 'App',
     components: {
       Tab, BookHeader
     },
-    data(){
+    data() {
       return {
-        index:'',
-        names:''
+        index: '',
+        names: ''
       }
     },
-    watch:{
-      $route(to,from) {
-        if(to.meta.index > from.meta.index) {
-          this.names='right'
+    watch: {
+      $route(to, from) {
+        if (to.meta.index > from.meta.index) {
+          this.names = 'right'
         } else {
           this.names = 'left'
         }
@@ -40,37 +40,44 @@
   }
 </script>
 <style scoped lang="less">
-  .right-enter{
-    transform:translateX(-100%);
+  .right-enter {
+    transform: translateX(-100%);
   }
-  .right-enter-to{
-    transform:translateX(0);
+
+  .right-enter-to {
+    transform: translateX(0);
   }
+
   .right-leave-to {
-     transform:translateX(100%);
-   }
-  .right-enter-active,.right-leave-active {
+    transform: translateX(100%);
+  }
+
+  .right-enter-active, .right-leave-active {
     transition: all .4s;
   }
 
-  .left-enter{
-    transform:translateX(100%);
+  .left-enter {
+    transform: translateX(100%);
   }
-  .left-enter-to{
-    transform:translateX(0);
+
+  .left-enter-to {
+    transform: translateX(0);
   }
+
   .left-leave-to {
-    transform:translateX(-100%);
+    transform: translateX(-100%);
   }
-  .left-enter-active,.left-leave-active {
+
+  .left-enter-active, .left-leave-active {
     transition: all .4s;
   }
+
   /*.slide-leave {*/
-    /*transform: translate3d(100%,0,0);*/
+  /*transform: translate3d(100%,0,0);*/
   /*}*/
   /*.slide-leave-to {*/
-    /*transform: translate3d(0);*/
+  /*transform: translate3d(0);*/
   /*}*/
-  
-  
+
+
 </style>
